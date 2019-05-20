5cbfc8e9564a9e45678ca763cfbe069ecd351517
sfijan <sven.j.fijan@gmail.com>
2019-05-20 17:08:29 +0200

    competition, task view and add working

--- app/webapp/forms.py
+++ app/webapp/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, DateTimeField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, DateTimeField, BooleanField, IntegerField, SelectMultipleField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, NumberRange
-#from webapp.models import *
+from webapp.models import User, Competition, Task
+from datetime import datetime
+
+
+class NotValidatedSelectMultipleField(SelectMultipleField):
+    def pre_validate(self, form):
+        pass
 
 
 class RegistrationForm(FlaskForm):
@@ -32,28 +38,44 @@ class LoginForm(FlaskForm):
                              validators=[DataRequired()])
     submit = SubmitField('Log in')
 
+    def validate_username(self, username):
+        if not User.select().where(User.username == username.data).exists():
+            raise ValidationError("Username '" + username.data + "' does not exist")
+
 
 class TaskAddForm(FlaskForm):
     title = StringField('Task title',
                        validators=[DataRequired(), Length(min=1, max=64)])
-    maxpoints = IntegerField('Maximum points', validators=[NumberRange(min=1)])
-    text = FileField('File with task text', validators=[FileAllowed(['pdf', 'txt', ''])])
+    text = FileField('File with task text (.pdf or .txt)',
+                     validators=[FileAllowed(['pdf', 'txt']), DataRequired()])
+    #TODO fix file
+    maxpoints = IntegerField('Maximum points',
+                             validators=[DataRequired(), NumberRange(min=1)])
     public = BooleanField('Public')
     submit = SubmitField('Add task')
+
+    def validate_title(self, title):
+        if Task.select().where(Task.title == title.data).exists():
+            raise ValidationError("Task '" + title.data + "' already exist")
 
 
 class CompetitionAddForm(FlaskForm):
     name = StringField('Competition name',
                        validators=[DataRequired(), Length(min=1, max=64)])
-    start_time = DateTimeField('Start time', validators=[DataRequired()])
-    end_time = DateTimeField('End time', validators=[DataRequired()])
-    public = BooleanField('Public')
+    start_time = DateTimeField('Start time', validators=[DataRequired()], format="%d.%m.%Y %H:%M")
+    end_time = DateTimeField('End time', validators=[DataRequired()], format="%d.%m.%Y %H:%M")
+    public = BooleanField('Public:')
+    tasks = NotValidatedSelectMultipleField('Tasks', choices=[(t.id, t.title) for t in Task.select()])
     submit = SubmitField('Add competition')
 
     def validate_start_time(self, start_time):
         if start_time.data < datetime.now():
-            raise ValidationError('Start time must be after now')
+            raise ValidationError('Start time must be in the future')
 
     def validate_end_time(self, end_time):
-        if end_time.data < start_time.data:
+        if end_time.data <= self.start_time.data:
             raise ValidationError('End time must be after start time')
+
+    def validate_name(self, name):
+        if Competition.select().where(Competition.name == name.data).exists():
+            raise ValidationError("Competition '" + name.data + "' already exist")
--- app/webapp/routes.py
+++ app/webapp/routes.py
@@ -1,9 +1,10 @@
 from flask import render_template, url_for, flash, redirect, request
-from webapp.forms import RegistrationForm, LoginForm, CompetitionAddForm
-from webapp.models import *
+from webapp.forms import RegistrationForm, LoginForm, CompetitionAddForm, TaskAddForm
+from webapp.models import User, Competition, Task, Contains
 from webapp import app, database, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import *
+import os
 
 
 @app.route('/')
@@ -14,37 +15,72 @@ def home():
 @app.route('/calendar')
 def calendar():
     competitions = Competition.select().where(Competition.end_time > datetime.now())
-    return render_template('calendar.html', title='calendar', competitions=competitions)
+    return render_template('calendar.html', title='calendar', competitions=competitions, current_user=current_user)
 
 
-@app.route('/competition/<competition_id>')
+@app.route('/competition/<competition_id>', methods=['GET', 'POST'])
 def display_competition(competition_id):
-    competition = Competition.get(competition_id)
+    competition = Competition.get(int(competition_id))
     return render_template('competition.html', title=competition.name, competition=competition, Contains=Contains)
 
 
-@app.route('/competition/add')
-def add_competition():      #TODO popravim template
+@app.route('/competition/add', methods=['GET', 'POST'])
+def add_competition():
     if not (current_user.is_authenticated and current_user.admin):
         flash('You do not have admin privilages!', 'danger')
         return redirect(url_for('home'))
     form = CompetitionAddForm()
-    return render_template('add-competition.html', title='Add competition', form=form)
+    if form.validate_on_submit():
+        competition = Competition(name=form.name.data,
+                                  start_time=form.start_time.data,
+                                  end_time=form.end_time.data,
+                                  public=form.public.data)
+        competition.save()
+        print(form.tasks.data)
+        for task in form.tasks.data:
+            contains = Contains(competition=competition.id,
+                                task=int(task)).save()
+        #print(form.tasks.getlist('form'))
+        flash('Competition added seccesfully!', 'success')
+        return redirect(url_for('calendar'))
+    return render_template('add_competition.html', title='Add competition', form=form)
 
 
-@app.route('/task/add')
+@app.route('/task')
+def task():
+    tasks = Task.select()
+    return render_template('list_task.html', title='task', Task=Task, current_user=current_user)
+
+
+def save_text(form):
+    _, ext = os.path.splitext(form.text.data.filename)
+    filename = form.title.data.replace(' ', '_') + ext
+    route = os.path.join(app.root_path, 'static/task_text', filename)
+    form.text.data.save(route)
+    return os.path.join('static/task_text', filename)
+
+
+@app.route('/task/add', methods=['GET', 'POST'])
 def add_task():
     if not (current_user.is_authenticated and current_user.admin):
         flash('You do not have admin privilages!', 'danger')
         return redirect(url_for('home'))
     form = TaskAddForm()
+    if form.validate_on_submit():
+        relative_path = save_text(form)
+        task = Task(title=form.title.data,
+                    text=relative_path,
+                    maxpoints=form.maxpoints.data,
+                    public=form.public.data).save()
+        flash('Task added seccesfully!', 'success')
+        return redirect(url_for('task'))
     return render_template('add_task.html', title='Add task', form=form)
 
 
 @app.route('/task/<task_id>')
 def display_task(task_id):
     selected_task = Task.get(int(task_id))
-    return render_template('task.html', title=selected_task.title, task=selected_task)
+    return render_template('view_task.html', title=selected_task.title, task=selected_task, file=selected_task.text)
 
 
 @app.route('/register', methods=['GET', 'POST'])
