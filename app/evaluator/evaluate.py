37efbd758786401f40ccf3fc6b6f672176ec819e
sfijan <sven.j.fijan@gmail.com>
2019-06-08 14:11:02 +0200

    added fixed time limit of 1s

--- app/evaluator/evaluate.py
+++ app/evaluator/evaluate.py
@@ -1,12 +1,21 @@
 from zipfile import ZipFile
+import time
+import multiprocessing
 import subprocess
 import os
+
+
 
 def evaluate(program, tests, language):
     #program: name of the program file
     #test: zip archive with the tests
     #language: name of language from Language
 
+    #function for running the programm
+    def run():
+        submission_output = subprocess.check_output('echo -e "' + z.open(infiles[i]).read().decode() +
+                                                    '" | ./selector.sh "'+language+'" '+program,
+                                                    shell=True).decode()
     z = ZipFile(tests)
     infiles = []
     outfiles = []
@@ -22,9 +31,13 @@ def evaluate(program, tests, language):
                 outfiles.append(file)
 
     for i in range(len(infiles)):
-        submission_output = subprocess.check_output('echo -e "' + z.open(infiles[i]).read().decode() +
-                            '" | ./selector.sh "'+language+'" '+program,
-                            shell=True).decode()
+        p = multiprocessing.Process(target=run, name="Foo", args=())
+        p.start()
+        p.join(1)       #TODO make this the time limit constraint
+        if p.is_alive():
+            p.terminate()
+            p.join()
+
         correct_output = z.open(outfiles[i]).read().decode()
         result.append((infiles[i].split('/')[1], submission_output.strip(' \n\t') == correct_output.strip(' \n\t')))
         #TODO change first element to zip file path
