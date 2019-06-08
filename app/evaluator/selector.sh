8b3b08c29033d40e339dcad802f97c3692999d45
sfijan <sven.j.fijan@gmail.com>
2019-06-08 14:10:41 +0200

    added comments

--- app/evaluator/selector.sh
+++ app/evaluator/selector.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+#this script compiles the given code (as required by the language) and executes it
+#first cl argument	$1: the programming language that the code is written in
+#second cl argument	$2: the file with the program
+
 if [ "$1" == "python 2.7" ]
 then
     python2.7 $2
